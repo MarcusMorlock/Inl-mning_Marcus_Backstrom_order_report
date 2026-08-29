3ea36f65635fa3b52eb6fff9fb3e004643871e2e
Marcus Backstrom <VBMarcusVB@hotmail.com>
2026-08-29 21:46:58 +0200

    started

--- src/order_report/validation.py
+++ src/order_report/validation.py
@@ -1,1 +1,18 @@
+import pandas as pd
 
+
+def validate_order_df(df: pd.DataFrame) -> pd.DataFrame:
+        
+    required = {
+        "order_id",
+        "order_date",
+        "customer_id",
+        "region",
+        "product_category",
+        "quantity",
+        "unit_price",
+        "discount",
+        "returned",
+    }
+
+    return df
