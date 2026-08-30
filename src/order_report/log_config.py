2b5f5065b5e183d1c4314db6a0d0e2fa9059f676
Marcus Backstrom <VBMarcusVB@hotmail.com>
2026-08-30 14:15:12 +0200

    config inital done

--- src/order_report/log_config.py
+++ src/order_report/log_config.py
@@ -8,10 +8,35 @@ LOGGER_NAME ="order_report"
 
 def configure_log() -> None:
     """"""
+    order_logger = logging.getLogger(LOGGER_NAME)
 
+    if order_logger.handlers:
+        return
 
+    order_logger.setLevel(logging.WARNING)
+    order_logger.propagate = False
 
-    return None
+    formatter = logging.Formatter(
+        "%(asctime)s | %(levelname)s | "
+        "%(name)s | %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    console_handler.setFormatter(formatter)
+
+    file_handler = logging.FileHandler(
+        "order_report.log",
+        encoding="utf-8",
+    )
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(formatter)
+
+    order_logger.addHandler(console_handler)
+    order_logger.addHandler(file_handler)
+
+    
 
 
 
